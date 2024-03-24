6e3dadd901cfd316db7abfaf6c52462cadd2e5ee
YusafS94 <yusafsaddiq@gmail.com>
2024-03-24 00:24:35 +0000

    Title link and pagination for categories fixed

--- category.php
+++ category.php
@@ -1,6 +1,6 @@
 <?php
-	get_header();
-?>
+   get_header();
+   ?>
     
 		<article class="content px-3 py-5 p-md-5">
 
@@ -8,10 +8,9 @@
 		if(have_posts()){
 			while(have_posts()){
 				the_post();
-				
-
                 get_template_part('template-parts/content', 'archive');
 			}
+            the_posts_pagination();
 		}
 		?>
 
